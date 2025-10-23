cbf14993b8b4d7c2b0b78d6f46a16017f95b28c1
Finlay <finlay.mitchner@gmail.com>
2025-10-23 09:33:09 -0700

    done

--- assignment3.py
+++ assignment3.py
@@ -23,12 +23,12 @@ def CentralTendency(*args):
     mylist.sort()
     length = len(mylist)
     if len(mylist) % 2 == 0:
-        median = mylist[(length//2)] + mylist[(length//2+1)]
-    else:
+        median = (mylist[int(length/2-1)] + mylist[int(length/2)])/2
+    elif len(mylist) % 2 != 0:
         median = mylist[math.ceil(length/2)]
     midpoint = (mylist[0] + mylist[length-1])/2
     print(median,mode,midpoint)
     return median, mode, midpoint
 
-assert CentralTendency(8, 6, 6, 4, 6, 7, 10, 3, 1, 3, 9, 7, 9, 6, 5, 6, 5, 1, 6, 3) == (4.5,6,5.5)
-assert CentralTendency(5, 5, 8, 10, 4, 7, 5, 6, 9, 9, 5, 4, 7, 7, 3, 8, 2, 3, 5, 9) == (5,5,6)
+assert CentralTendency(8, 6, 6, 4, 6, 7, 10, 3, 1, 3, 9, 7, 9, 6, 5, 6, 5, 1, 6, 3) == (6,6,5.5)
+assert CentralTendency(5, 5, 8, 10, 4, 7, 5, 6, 9, 9, 5, 4, 7, 7, 3, 8, 2, 3, 5, 9) == (5.5,5,6)
--- test.py
+++ test.py
@@ -1,7 +1,6 @@
-def test(*c):
-   print(len(c))
-   print(c)
-   print(c[1])
+def test():
+   mylist = [5, 5, 8, 10, 4, 7, 5, 6, 9, 9, 5, 4, 7, 7, 3, 8, 2, 3, 5, 9]
+   mylist.sort()
+   print(mylist)
 
-   return 
-test(7,8,4,9)
+test()
